bb2f7a7266d1cb52d0fb24b1c4e49832a9976e58
Clovis Junior <clovis.sa.junior@gmail.com>
2021-08-16 16:24:17 -0300

    Otimização no código
    Um modo mais fácil de ler o código.

--- commands/users/avatar.js
+++ commands/users/avatar.js
@@ -1,29 +1,26 @@
 const { MessageEmbed }=require('discord.js');
+const model = user => {
+    let embed = new MessageEmbed()
+        .setColor("#0000FF")
+        .setTitle(`Avatar de ${user.tag}`)
+        .setURL(user.avatarURL({size: 2048, dynamic: true}))
+        .setImage(user.avatarURL({size: 2048, dynamic: true}));
+
+    return embed
+};
 
 module.exports = {
     name: 'avatar',
     description: 'Envia o avatar da(s) pessoa(s) mencionada(s)',
+    
     execute({message}) {
         message.delete({timeout: 30});
         
-        if(message.mentions.users.size){
-            let index = 0, embed = [];
-            message.mentions.users.forEach(user => {
-                embed[index] = new MessageEmbed()
-                    .setColor("#0000FF")
-                    .setTitle(`Avatar de ${user.tag}`)
-                    .setURL(user.avatarURL({size: 2048, dynamic: true}))
-                    .setImage(user.avatarURL({size: 2048, dynamic: true}));
-                message.channel.send({ embeds: [embed[index]] });
-                index++
-            })
-        }else{
-            const embed = new MessageEmbed()
-                .setColor("#0000FF")
-                .setURL(message.author.avatarURL({size: 2048, dynamic: true}))
-                .setImage(message.author.avatarURL({size: 2048, dynamic: true}));
-            return message.channel.send({ embeds: [embed] })
-        }
-        
+        if(message.mentions.users.size) {
+            message.mentions.users.forEach(user=>{
+                message.channel.send({ embeds: [model(user)] })
+            });
+        }else
+            message.channel.send({ embeds: [model(message.author)] });
     }
 }
