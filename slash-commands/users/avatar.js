82f2fb0485d302b6d4a3e14ebb84f00368b374ae
Clovis Junior <clovis.sa.junior@gmail.com>
2021-08-16 16:27:53 -0300

    Otimização e facilidade de ler o código

--- slash-commands/users/avatar.js
+++ slash-commands/users/avatar.js
@@ -1,6 +1,16 @@
 const { MessageEmbed } = require('discord.js')
 const { manager } = require('../../utils/manager')
 
+const model = user => {
+    let embed=new MessageEmbed()
+        .setColor(0x0000FF)
+        //.setTitle(manager.logger("avatar", null, null, null, user))
+        .setURL(user.avatarURL({size: 2048, dynamic: true}))
+        .setImage(user.avatarURL({size: 2048, dynamic: true}));
+
+    return embed
+}
+
 module.exports = {
     name: "avatar",
     description: "teste",
@@ -9,22 +19,14 @@ module.exports = {
         if (int.options.data.map(({user}) => user).length >= 1) {
             await int.deferReply()
 
-            let i = 0, embed = [];
             await int.options.data.map(({user}) => user).forEach(user => {
-                embed[i] = new MessageEmbed()
-                    .setColor(0x0000FF)
-                    .setTitle(manager.logger("avatar", null, null, null, user))
-                    .setURL(user.avatarURL({size: 2048, dynamic: true}))
-                    .setImage(user.avatarURL({size: 2048, dynamic: true}));
-                int.followUp({ embeds: [embed[i]] })
-                i++
+                int.followUp({ embeds: [model(user)] })
             });
-
         } else {
             const embed = new MessageEmbed()
                 .setColor(0x0000FF)
                 .setImage(int.user.avatarURL({size: 2048, dynamic: true}));
-            return await int.reply( {embeds: [embed] })
+            return await int.reply( {embeds: [model(int.user)] })
         }
     }
 }
